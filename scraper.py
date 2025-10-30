742ccba9d65f034cfafefb8fddae74041508230a
Mykolazaz <mykolas.motiejunas@gmail.com>
2025-10-31 00:00:23 +0200

    Scraper goes to the last page

--- scraper.py
+++ scraper.py
@@ -8,6 +8,8 @@ driver = webdriver.Chrome(service=cService)
 
 driver.get('https://www.aruodas.lt/')
 
+time.sleep(1)
+
 # Decline all cookies
 driver.find_element(By.ID, 'onetrust-reject-all-handler').click()
 
@@ -17,7 +19,16 @@ driver.find_elements(By.ID, 'display_FRegion')[0].click()
 # Click on city Vilnius
 driver.find_element(By.CSS_SELECTOR, 'label.dropDownLabel[for="input_FRegion_461"]').click()
 
-time.sleep(5)
+# Click 'Search' button
+driver.find_element(By.ID, 'buttonSearchForm').click()
+
+# Click advert list order button and choose the second option
+driver.find_element(By.XPATH, '//*[@id="changeListOrder"]/option[2]').click()
+
+# Go to the last page
+driver.find_element(By.XPATH, '/html/body/div[1]/div[4]/div[1]/div[9]/a[7]').click()
+
+time.sleep(10)
 
 
 
